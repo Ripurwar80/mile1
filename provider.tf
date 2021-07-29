49aca84c977afe50f055eaece392ed40d4b57f3a
JasjitSinghRudra <50959696+JasjitSinghRudra@users.noreply.github.com>
2021-07-29 15:10:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@
 provider "aws" {
   profile                 = "default"
   region                  = var.aws_region
-  access_key = "AKIAVH24FXJXWRFFCROQ" #has to put
-  secret_key = "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" #has to put
+  access_key = "" #has to put
+  secret_key = "" #has to put
   #i'm trying to bind them in a variable and pass for the security purpose
 }
