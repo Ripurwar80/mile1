c08b5f51f2abca4a4517dab418a1c4e9809d938f
JasjitSinghRudra <50959696+JasjitSinghRudra@users.noreply.github.com>
2021-07-30 17:08:27 +0530

    Update logs.tf

--- logs.tf
+++ logs.tf
@@ -12,5 +12,5 @@ resource "aws_cloudwatch_log_group" "log_group" {
 
 resource "aws_cloudwatch_log_stream" "myapp_log_stream" {
   name           = "log-stream"
-  log_group_name = aws_cloudwatch_log_group.myapp_log_group.name
+  log_group_name = aws_cloudwatch_log_group.log_group.name
 }
